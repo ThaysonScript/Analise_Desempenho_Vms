8eafa9e8f56c8dbce2c66d0554233670a23e4567
ThaysonScript <thaysonguedes01@gmail.com>
2024-03-28 21:42:11 -0300

    +refact

--- analise_dados/fragmentacao/fragmentacao_start.py
+++ analise_dados/fragmentacao/fragmentacao_start.py
@@ -1,8 +1,8 @@
-from ler_csv import ler_csv
-from revisado.fragmentacao.configurar_index_data_frame import configurar_index_data_frame
-from revisado.fragmentacao.fazer_plotagem import fazer_plotagem
-from revisado.fragmentacao.filtrar_data_frame import filtrar_data_frame
-from revisado.fragmentacao.pivotar_data_frame import pivotar_data_frame
+from analise_dados.fragmentacao.ler_csv import ler_csv
+from analise_dados.fragmentacao.configurar_index_data_frame import configurar_index_data_frame
+from analise_dados.fragmentacao.fazer_plotagem import fazer_plotagem
+from analise_dados.fragmentacao.filtrar_data_frame import filtrar_data_frame
+from analise_dados.fragmentacao.pivotar_data_frame import pivotar_data_frame
 
 
 def fragmentacao_start():
--- analise_dados/main.py
+++ analise_dados/main.py
@@ -1,2 +1,9 @@
+from monitoramentos.monitoramentos_start import monitoramentos_start
+from fragmentacao.fragmentacao_start import fragmentacao_start
+
 def main():
-    pass
+    fragmentacao_start()
+    monitoramentos_start()
+    
+if __name__ == '__main__':
+    main()
